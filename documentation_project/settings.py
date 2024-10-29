abae0387a4fd41a0742f2abca010a60fb589e696
Christophe POVEREAU <cpovereau@ocealia-informatique.com>
2024-10-29 13:09:00 +0100

    Trigger GitHub Actions run

--- documentation_project/settings.py
+++ documentation_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,8 +143,6 @@ REST_FRAMEWORK = {
 }
 
 # settings.py
-
-import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
