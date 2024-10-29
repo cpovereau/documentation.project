826bd1fa792849bab92f1d98446a8073142acc84
Christophe POVEREAU <cpovereau@ocealia-informatique.com>
2024-10-29 17:35:31 +0100

    Test CircleIT

--- documentation_project/views.py
+++ documentation_project/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Bienvenue sur la page d'accueil !")
+    return HttpResponse("Bienvenue sur la page d'accueil - je teste !")
