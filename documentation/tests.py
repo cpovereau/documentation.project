c6b577bce469d5f8051bc44daa27f20a4f8cab76
Christophe POVEREAU <cpovereau@ocealia-informatique.com>
2024-10-31 15:04:04 +0100

    Test Authentification

--- documentation/tests.py
+++ documentation/tests.py
@@ -5,13 +5,23 @@ from django.contrib.auth.models import User
 
 class AuthenticationTests(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='gitadmin', password='Ocealia31520', first_name='Test', last_name='Mon', is_superuser=False, is_staff=False, is_active=True)
+        # Crée un utilisateur de test avec les attributs nécessaires
+        self.user = User.objects.create_user(
+            username='gitadmin', 
+            password='Ocealia31520',
+            first_name='Test', 
+            last_name='User', 
+            is_superuser=False, 
+            is_staff=False, 
+            is_active=True
+        )
 
     def test_login(self):
         url = reverse('login')
         data = {'username': 'gitadmin', 'password': 'Ocealia31520'}
-        response = self.client.post(url, data, content_type='application/json')
-        print(response.data)
+        response = self.client.post(url, data, format='json')
+        print(response.data)  # Affiche la réponse pour débogage
+        # Vérifie le statut HTTP et la présence du nom d'utilisateur dans la réponse
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('username', response.data)
 
@@ -19,4 +29,5 @@ class AuthenticationTests(APITestCase):
         self.client.login(username='gitadmin', password='Ocealia31520')
         url = reverse('logout')
         response = self.client.post(url)
+        # Vérifie le statut HTTP pour la déconnexion
         self.assertEqual(response.status_code, status.HTTP_200_OK)
